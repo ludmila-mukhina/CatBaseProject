e0781689dc3bf9db04eb33e99e115a30b88b6303
lyudmila.archackowa <lyudmila.archackowa@yandex.ru>
2022-11-14 13:49:36 +0300

    Добавление изображений в БД. Создание галереи изображений

--- WpfApp2/Pages/PersonalPage.xaml.cs
+++ WpfApp2/Pages/PersonalPage.xaml.cs
@@ -24,26 +24,34 @@ namespace WpfApp2
     public partial class PersonalPage : Page
     {
         UserTable user;  // создаем объект для хранения информации о пользователе
+
+        // метод для отображения изображения в личном кабинете. первый аргумент - байтовый массив, в котором хранится изображение в БД, второй аргумент - имя изображения в разметке
+        void showImage(byte[] Barray, System.Windows.Controls.Image img)
+        {
+                BitmapImage BI = new BitmapImage();  // создаем объект для загрузки изображения
+                using (MemoryStream m = new MemoryStream(Barray))  // для считывания байтового потока
+                {
+                    BI.BeginInit();  // начинаем считывание
+                    BI.StreamSource = m;  // задаем источник потока
+                    BI.CacheOption = BitmapCacheOption.OnLoad;  // переводим изображение
+                    BI.EndInit();  // заканчиваем считывание
+                }
+            img.Source = BI;  // показываем картинку на экране (imUser – имя картиник в разметке)
+            img.Stretch = Stretch.Uniform;
+        }
+
         public PersonalPage(UserTable user)
         {
             InitializeComponent();
             this.user = user;  // заполняем выше созданный объект информацией об авторизованном пользователе
             tbName.Text = user.Name;  // заполняем поле с именем
             tbSurname.Text = user.Surname;  // заполняем поле с фамилией
-            Userphoto u = BaseClass.tBE.Userphoto.FirstOrDefault(x => x.idUser == user.idUser); // для загрузки картинки находим фото пользователя в таблице, где хранятся фото
-            if (u != null)  // если объект не пустой, начинает переводить байтовый массив в изображение
+            List<Userphoto> u = BaseClass.tBE.Userphoto.Where(x => x.idUser == user.idUser).ToList(); // для загрузки картинки находим все фото пользователя в таблице, где хранятся фото
+            if (u != null)  // если список с фото не пустой, начинает переводить байтовый массив в изображение
             {
-                byte[] Bar = u.photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
-                BitmapImage BI = new BitmapImage();  // создаем объект для загрузки изображения
-                using (MemoryStream m = new MemoryStream(Bar))  // для считывания байтового потока
-                {
-                    BI.BeginInit();  // начинаем считывание
-                    BI.StreamSource = m;  // задаем источник потока
-                    BI.CacheOption = BitmapCacheOption.OnLoad;  // переводим изображение
-                    BI.EndInit();  // заканчиваем считывание
-                } 
-                imUser.Source = BI;  // показываем картинку на экране (imUser – имя картиник в разметке)
-                imUser.Stretch = Stretch.Uniform;
+
+                byte[] Bar = u[u.Count-1].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных) - выбираем последнее добавленное изображение
+                showImage(Bar, imUser);  // отображаем картинку
             }
         }
 
@@ -64,7 +72,8 @@ namespace WpfApp2
                 u.idUser = user.idUser;  // присваиваем значение полю idUser (id авторизованного пользователя)
 
                 OpenFileDialog OFD = new OpenFileDialog();  // создаем диалоговое окно
-                OFD.ShowDialog();  // открываем диалоговое окно
+                //OFD.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);  // выбор папки для открытия
+                OFD.ShowDialog();  // открываем диалоговое окно             
                 string path = OFD.FileName;  // считываем путь выбранного изображения
                 System.Drawing.Image SDI = System.Drawing.Image.FromFile(path);  // создаем объект для загрузки изображения в базу
                 ImageConverter IC = new ImageConverter();  // создаем конвертер для перевода картинки в двоичный формат
@@ -80,6 +89,96 @@ namespace WpfApp2
             {
                 MessageBox.Show("Что-то пошло не так");
             }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog OFD = new OpenFileDialog();  // создаем диалоговое окно
+                OFD.Multiselect = true;  // открытие диалогового окна с возможностью выбора нескольких элементов
+                if (OFD.ShowDialog() == true)  // пока диалоговое окно открыто, будет в цикле записывать каждое выбранное изображение в БД
+                {
+                    foreach (string file in OFD.FileNames)  // цикл организован по именам выбранных файлов
+                    {
+                        Userphoto u = new Userphoto();  // создание объекта для добавления записи в таблицу, где хранится фото
+                        u.idUser = user.idUser;  // присваиваем значение полю idUser (id авторизованного пользователя)
+                        string path = file;  // считываем путь выбранного изображения
+                        System.Drawing.Image SDI = System.Drawing.Image.FromFile(file);  // создаем объект для загрузки изображения в базу
+                        ImageConverter IC = new ImageConverter();  // создаем конвертер для перевода картинки в двоичный формат
+                        byte[] Barray = (byte[])IC.ConvertTo(SDI, typeof(byte[]));  // создаем байтовый массив для хранения картинки
+                        u.photoBinary = Barray;  // заполяем поле photoBinary полученным байтовым массивом
+                        BaseClass.tBE.Userphoto.Add(u);  // добавляем объект в таблицу БД
+                    }
+                    BaseClass.tBE.SaveChanges();
+                    MessageBox.Show("Фото добавлены");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        int n = 0;
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            spGallery.Visibility = Visibility.Visible;
+            List<Userphoto> u = BaseClass.tBE.Userphoto.Where(x => x.idUser == user.idUser).ToList();
+            if (u != null)  // если объект не пустой, начинает переводить байтовый массив в изображение
+            {
+
+                byte[] Bar = u[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
+                showImage(Bar, imgGallery);  // отображаем картинку
+            }
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            List<Userphoto> u = BaseClass.tBE.Userphoto.Where(x => x.idUser == user.idUser).ToList();
+            n++;
+            if (Back.IsEnabled == false)
+            {
+                Back.IsEnabled = true;
+            }
+            if (u != null)  // если объект не пустой, начинает переводить байтовый массив в изображение
+                {
+
+                    byte[] Bar = u[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
+                    showImage(Bar, imgGallery);              
+                }
+            if (n == u.Count-1)
+            {
+                Next.IsEnabled = false;
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            List<Userphoto> u = BaseClass.tBE.Userphoto.Where(x => x.idUser == user.idUser).ToList();
+            n--;
+            if (Next.IsEnabled==false)
+            {
+                Next.IsEnabled = true;
+            }
+            if (u != null)  // если объект не пустой, начинает переводить байтовый массив в изображение
+            {
+
+                byte[] Bar = u[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
+                BitmapImage BI = new BitmapImage();  // создаем объект для загрузки изображения
+                showImage(Bar, imgGallery);
+            }
+            if (n == 0)
+            {
+                Back.IsEnabled = false;
+            }
+        }
+
+        private void btnOld_Click(object sender, RoutedEventArgs e)
+        {
+            List<Userphoto> u = BaseClass.tBE.Userphoto.Where(x => x.idUser == user.idUser).ToList();
+            byte[] Bar = u[n].photoBinary;   // считываем изображение из базы (считываем байтовый массив двоичных данных)
+            showImage(Bar, imUser);  // отображаем картинку
         }
     }
 }
